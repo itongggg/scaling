cbc927ac67dbf305c16208cf856d0475fef95efa
itongggg <itongggg@163.com>
2023-12-06 10:16:51 +0800

    add mask for attn

--- test/test_attn.py
+++ test/test_attn.py
@@ -5,35 +5,27 @@ from typing import Optional, Tuple
 
 
 class CausalSelfAttention(nn.Module):
-    def __init__(self, config: LLaMAConfig, num_new_dim: int = None) -> None:
+    def __init__(self, n_embd, n_head, num_new_dim: int = None) -> None:
         super().__init__()
         self.num_new_dim = num_new_dim
-        assert config.n_embd % config.n_head == 0
+        assert n_embd % n_head == 0
 
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=False)
-        # self.Q = nn.Linear(config.n_embd, config.n_embd, bias=False)
-        # self.K = nn.Linear(config.n_embd, config.n_embd, bias=False)
-        # self.V = nn.Linear(config.n_embd, config.n_embd, bias=False)
-        # output projection
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=False)
+        self.c_attn = nn.Linear(n_embd, 3 * n_embd, bias=False)
+        self.c_proj = nn.Linear(n_embd, n_embd, bias=False)
 
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.block_size = config.block_size
+        self.n_head = n_head
+        self.n_embd = n_embd
         if num_new_dim is not None:
-            self.c_mask = torch.ones(config.n_embd).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+            self.cproj_mask = torch.ones(n_embd).unsqueeze(0).unsqueeze(0)
+            self.cproj_mask[:, :, -num_new_dim:] = 0
         else:
-            self.c_mask = 1
+            self.cproj_mask = 1
 
     def forward(
         self,
         x: torch.Tensor,
-        rope: RoPECache,
         mask: MaskCache,
-        max_seq_length: int,
-        input_pos: Optional[torch.Tensor] = None,
-        kv_cache: Optional[KVCache] = None,
         stage_1: bool = False,
     ) -> Tuple[torch.Tensor, Optional[KVCache]]:
         B, T, C = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
@@ -48,35 +40,19 @@ class CausalSelfAttention(nn.Module):
             k[:,:,-self.num_new_dim:] = 0
             v[:,:,-self.num_new_dim:] = 0
 
-            k = k.view(B, T, self.n_head, head_size)
-            q = q.view(B, T, self.n_head, head_size)
-            v = v.view(B, T, self.n_head, head_size)
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        else:
+            q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
 
         
         k = k.view(B, T, self.n_head, head_size)
         q = q.view(B, T, self.n_head, head_size)
         v = v.view(B, T, self.n_head, head_size)
 
-        q = apply_rope(q, rope)
-        k = apply_rope(k, rope)
-
         k = k.transpose(1, 2)  # (B, nh, T, hs)
         q = q.transpose(1, 2)  # (B, nh, T, hs)
         v = v.transpose(1, 2)  # (B, nh, T, hs)
 
-        if kv_cache is not None:
-            cache_k, cache_v = kv_cache
-            # check if reached token limit
-            if input_pos[-1] >= max_seq_length:
-                input_pos = torch.tensor(max_seq_length - 1, device=input_pos.device)
-                # shift 1 position to the left
-                cache_k = torch.roll(cache_k, -1, dims=2)
-                cache_v = torch.roll(cache_v, -1, dims=2)
-            k = cache_k.index_copy(2, input_pos, k)
-            v = cache_v.index_copy(2, input_pos, v)
-            kv_cache = k, v
 
         # efficient attention using Flash Attention CUDA kernels
         y = F.scaled_dot_product_attention(q, k, v, attn_mask=mask, dropout_p=0.0)
@@ -85,5 +61,16 @@ class CausalSelfAttention(nn.Module):
 
         # output projection
         y = self.c_proj(y)
+        if stage_1:
+            y = y * self.cproj_mask
 
-        return y, kv_cache
+        return y
+    
+orig_attn = CausalSelfAttention(128*3, 3)
+x = torch.randn((1, 4, 128*3))
+y = torch.randn((1, 4, 128*4))
+y[:, :, :128*3] = x
+y[:, :, -128:] = 0
+new_attn = CausalSelfAttention(128*4, 4, num_new_dim=128)
+new_attn.c_attn.weight.data[:128*3*3, :128*3] = orig_attn.c_attn.weight.data
+new_attn.c_proj.weight.data[:128*3, :128*3] = orig_attn.c_proj.weight.data
